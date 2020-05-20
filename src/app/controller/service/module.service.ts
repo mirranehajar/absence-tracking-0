b36a409de75791b82e893f77fe1c0d2370996799
mirranehajar <mirranehajar@gmail.com>
2020-05-20 14:52:02 +0000

    module

--- src/app/controller/service/module.service.ts
+++ src/app/controller/service/module.service.ts
@@ -1,9 +1,10 @@
-import {Injectable} from '@angular/core';
-import {Module, TypeSeance} from '../model/module';
 import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Enseignant} from '../model/enseignant.model';
+import {Module, TypeSeance} from '../model/module';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class ModuleService {
@@ -17,31 +18,52 @@ export class ModuleService {
   private _modules: Module[];
   // tslint:disable-next-line:variable-name
   private _moduleFounded: Module;
+  private _enseignant: Enseignant;
+  private _enseignants: Enseignant[];
   // tslint:disable-next-line:variable-name
   private _url = 'http://localhost:8090/absence-tracking/module/';
   // tslint:disable-next-line:variable-name
   private _urlT = 'http://localhost:8090/absence-tracking/typeSeance/';
+  private _urlE = 'http://localhost:8090/absence-tracking/enseignant/';
+
+  private ensaignantFounded: Enseignant;
   constructor(private http: HttpClient) { }
 
   public findByLibelle(module: Module) {
     this.http.get<Module>(this._url + 'libelle/' + module.libelle).subscribe(
-      data => {
+      (data) => {
         this.moduleFounded = data;
-      }
+      },
+    );
+  }
+
+  public findByLastName(enseignant: Enseignant) {
+    this.http.get<Enseignant>(this._url + 'lastName/' + enseignant.lastName).subscribe(
+      (data) => {
+        this.ensaignantFounded = data;
+      },
     );
   }
   public findAll() {
-    this.http.get<Array<Module>>(this._url).subscribe(
-      data => {
+    this.http.get<Module[]>(this._url).subscribe(
+      (data) => {
         this.modules = data;
-      }
+      },
     );
   }
   public findAllT() {
-    this.http.get<Array<TypeSeance>>(this._urlT).subscribe(
-      data => {
+    this.http.get<TypeSeance[]>(this._urlT).subscribe(
+      (data) => {
         this.typeSeances = data;
-      }
+      },
+    );
+  }
+
+  public findAllE() {
+    this.http.get<Enseignant[]>(this._urlT).subscribe(
+      (data) => {
+        this.enseignants = data;
+      },
     );
   }
   private clone(module: Module) {
@@ -51,7 +73,9 @@ export class ModuleService {
   }
   private cloneT(typeSeance: TypeSeance) {
     const mycloneT = new TypeSeance();
+    mycloneT.reference = typeSeance.reference;
     mycloneT.libelle = typeSeance.libelle ;
+    mycloneT.module.libelle = typeSeance.module.libelle;
     mycloneT.enseignant.lastName = typeSeance.enseignant.lastName;
     return mycloneT;
   }
@@ -62,7 +86,7 @@ export class ModuleService {
     return this._module;
   }
 
-  get modules(): Array<Module> {
+  get modules(): Module[] {
     if (this._modules == null) {
       this._modules = new Array<Module>();
     }
@@ -78,7 +102,7 @@ export class ModuleService {
   }
 
   // tslint:disable-next-line:adjacent-overload-signatures
-  set modules(value: Array<Module>) {
+  set modules(value: Module[]) {
     this._modules = value;
   }
 
@@ -87,8 +111,10 @@ export class ModuleService {
     this._moduleFounded = value;
   }
 
-
   get typeSeance(): TypeSeance {
+    if (this._typeSeance == null) {
+      this._typeSeance = new TypeSeance();
+    }
     return this._typeSeance;
   }
 
@@ -96,17 +122,41 @@ export class ModuleService {
     this._typeSeance = value;
   }
 
-
   get typeSeances(): TypeSeance[] {
+    if (this._typeSeances == null) {
+      this._typeSeances = new Array<TypeSeance>();
+    }
     return this._typeSeances;
   }
 
-  set typeSeances(value: Array<TypeSeance>) {
+  set typeSeances(value: TypeSeance[]) {
     this._typeSeances = value;
   }
 
+  get enseignant(): Enseignant {
+    if (this._enseignant == null) {
+      this._enseignant = new Enseignant();
+    }
+    return this._enseignant;
+  }
+
+  set enseignant(value: Enseignant) {
+    this._enseignant = value;
+  }
+
+  get enseignants(): Enseignant[] {
+    if (this._enseignants == null) {
+      this._enseignants = new Array<Enseignant>();
+    }
+    return this._enseignants;
+  }
+
+  set enseignants(value: Enseignant[]) {
+    this._enseignants = value;
+  }
+
   public deleteFromList(module: Module) {
-    const index = this.modules.findIndex(e => e.libelle === module.libelle);
+    const index = this.modules.findIndex((e) => e.libelle === module.libelle);
     if (index !== -1) {
       this.modules.splice(index, 1);
     }
@@ -114,10 +164,36 @@ export class ModuleService {
 
   deleteByLibelle(m: Module) {
     this.http.delete<number>(this._url + 'libelle/' + m.libelle).subscribe(
-      data => {
+      (data) => {
         console.log(data);
         this.deleteFromList(m);
-      }
+      },
+    );
+  }
+
+  saveModule() {
+    this.http.post<number>(this._url, this.module).subscribe(
+      (data) => {
+        if (data > 0) {
+          this.modules.push(this.clone(this.module));
+          this.module = null;
+        }
+      }, (error) => {
+        console.log('error');
+      },
+    );
+  }
+
+  saveTypeSeance() {
+    this.http.post<number>(this._url, this.typeSeance).subscribe(
+      (data) => {
+        if (data > 0) {
+          this.typeSeances.push(this.cloneT(this.typeSeance));
+          this.typeSeance = null;
+        }
+      }, (error) => {
+        console.log('error');
+      },
     );
   }
 }
